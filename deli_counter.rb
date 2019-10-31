ca351fc48ca6323f20ae424a45ad611a5c68d87a
Andres Carvajal <jc1993box@hotmail.com>
2019-10-31 04:53:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,29 @@
+katz_deli = []
+
 def line(katz_deli)
-  if katz_deli.length == 0
+  line_array = []
+  if katz_deli.empty? 
     puts "The line is currently empty."
   else
-    customers_line= []
-    katz_deli.each.with_index{|customer, index|customers_line<<"#{index=1}. #{customer}"
-    puts "The line is currently: #{customers_line.join(" "")}"
-    binding.pry
+    order = 1
+    katz_deli.each do |num|
+      line_array << "#{order}. #{num}"
+      order += 1 
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end 
