b9d90198141dcaca16364e4b7cbeff612130e679
Andres Carvajal <jc1993box@hotmail.com>
2019-10-31 04:52:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    customers_line= []
+    katz_deli.each.with_index{|customer, index|customers_line<<"#{index=1}. #{customer}"
+    puts "The line is currently: #{customers_line.join(" "")}"
+    binding.pry
+end
+end
